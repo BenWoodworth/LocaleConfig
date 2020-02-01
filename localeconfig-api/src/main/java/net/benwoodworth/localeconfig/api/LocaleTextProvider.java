17ce44517d15355e396e2e269c113d8b7daba343
BuildTools <unconfigured@null.spigotmc.org>
2020-02-01 12:42:01 -0500

    Broaden locale if unable to find a locale key.

--- localeconfig-api/src/main/java/net/benwoodworth/localeconfig/api/LocaleTextProvider.java
+++ localeconfig-api/src/main/java/net/benwoodworth/localeconfig/api/LocaleTextProvider.java
@@ -16,32 +16,59 @@ abstract class LocaleTextProvider {
     abstract Locale getServerLocale();
 
     @Nullable
-    LocaleText getText(@NotNull Locale locale, @NotNull String localeKey) {
-        int colonIndex = localeKey.indexOf(':');
-        if (colonIndex == -1) {
-            return getLocalText(locale, localeKey);
+    private static Locale broadenLocale(Locale locale) {
+        if (locale.getVariant().length() != 0) {
+            return new Locale(locale.getLanguage(), locale.getCountry());
+        } else if (locale.getCountry().length() != 0) {
+            return new Locale(locale.getLanguage());
+        } else {
+            return null;
         }
-
-        int nextColonIndex = localeKey.indexOf(':', colonIndex + 1);
-        if (nextColonIndex == -1) {
-            String namespace = localeKey.substring(0, colonIndex);
-            String namespacedLocaleKey = localeKey.substring(colonIndex + 1);
-
-            if (namespace.equals(getNamespace())) {
-                return getLocalText(locale, namespacedLocaleKey);
-            } else {
-                return getGlobalText(locale, namespace, namespacedLocaleKey);
-            }
-        }
-
-        throw new IllegalArgumentException("Locale key must have at most one colon for a namespace");
     }
 
     @Nullable
-    abstract LocaleText getLocalText(@NotNull Locale locale, @NotNull String localeKey);
+    private LocaleText getText(@NotNull Locale locale, @NotNull String localeKey) {
+        String namespace;
+        String namespacedLocaleKey;
+
+        int colonIndex = localeKey.indexOf(':');
+        if (colonIndex == -1) {
+            namespace = getNamespace();
+            namespacedLocaleKey = localeKey;
+        } else {
+            int nextColonIndex = localeKey.indexOf(':', colonIndex + 1);
+            if (nextColonIndex != -1) {
+                throw new IllegalArgumentException("Locale key must have at most one colon for a namespace");
+            }
+
+            namespace = localeKey.substring(0, colonIndex);
+            namespacedLocaleKey = localeKey.substring(colonIndex + 1);
+        }
+
+        Locale tryLocale = locale;
+        boolean triedEnglish = false;
+        do {
+            LocaleText localeText = getText(tryLocale, namespace, namespacedLocaleKey);
+            if (localeText != null) {
+                return localeText;
+            }
+
+            if (!triedEnglish && tryLocale.equals(Locale.ENGLISH)) {
+                triedEnglish = true;
+            }
+
+            tryLocale = broadenLocale(locale);
+        } while (tryLocale != null);
+
+        if (!triedEnglish) {
+            return getText(Locale.ENGLISH, namespace, namespacedLocaleKey);
+        } else {
+            return null;
+        }
+    }
 
     @Nullable
-    abstract LocaleText getGlobalText(@NotNull Locale locale, @NotNull String namespace, @NotNull String namespacedLocaleKey);
+    abstract LocaleText getText(@NotNull Locale locale, @NotNull String namespace, @NotNull String namespacedLocaleKey);
 
     private static class SoftLocaleTextProvider extends LocaleTextProvider {
         private String namespace;
@@ -58,25 +85,24 @@ abstract class LocaleTextProvider {
         }
 
         @Override
-        @NotNull Locale getServerLocale() {
+        @NotNull
+        Locale getServerLocale() {
             return Locale.ENGLISH;
         }
 
         @Override
-        @Nullable LocaleText getLocalText(@NotNull Locale locale, @NotNull String localeKey) {
-            Map<String, String> localeKeys = locales.get(locale);
-            if (localeKeys != null) {
-                String text = localeKeys.get(localeKey);
-                if (text != null) {
-                    return new LocaleText(locale, text);
+        @Nullable
+        LocaleText getText(@NotNull Locale locale, @NotNull String namespace, @NotNull String namespacedLocaleKey) {
+            if (getNamespace().equals(namespace)) {
+                Map<String, String> localeKeys = locales.get(locale);
+                if (localeKeys != null) {
+                    String text = localeKeys.get(namespacedLocaleKey);
+                    if (text != null) {
+                        return new LocaleText(locale, text);
+                    }
                 }
             }
 
-            return null;
-        }
-
-        @Override
-        @Nullable LocaleText getGlobalText(@NotNull Locale locale, @NotNull String namespace, @NotNull String namespacedLocaleKey) {
             return null;
         }
     }
