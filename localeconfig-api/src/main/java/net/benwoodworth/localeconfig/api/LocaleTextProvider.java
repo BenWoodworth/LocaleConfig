1a3096e1c992c6da2184b3c196648d1bafd985af
BuildTools <unconfigured@null.spigotmc.org>
2020-02-02 14:09:27 -0500

    Reformat code.

--- localeconfig-api/src/main/java/net/benwoodworth/localeconfig/api/LocaleTextProvider.java
+++ localeconfig-api/src/main/java/net/benwoodworth/localeconfig/api/LocaleTextProvider.java
@@ -3,7 +3,10 @@ package net.benwoodworth.localeconfig.api;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 abstract class LocaleTextProvider {
     private LocaleTextProvider() {
