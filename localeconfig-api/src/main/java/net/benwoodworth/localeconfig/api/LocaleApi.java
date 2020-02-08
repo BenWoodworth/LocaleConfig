22849eadf108576e45eed48ebed45d7064e3a3f2
BuildTools <unconfigured@null.spigotmc.org>
2020-02-07 23:27:38 -0500

    Rename LocaleApi.getText() to get().

--- localeconfig-api/src/main/java/net/benwoodworth/localeconfig/api/LocaleApi.java
+++ localeconfig-api/src/main/java/net/benwoodworth/localeconfig/api/LocaleApi.java
@@ -68,32 +68,32 @@ public class LocaleApi {
     }
 
     @Nullable
-    public static LocaleText getText(@NotNull String localeKey) {
-        return getText(getLocaleTextProvider().getServerLocale(), localeKey);
+    public static LocaleText get(@NotNull String localeKey) {
+        return get(getLocaleTextProvider().getServerLocale(), localeKey);
     }
 
     @Nullable
-    public static LocaleText getText(@NotNull Locale locale, @NotNull String localeKey) {
+    public static LocaleText get(@NotNull Locale locale, @NotNull String localeKey) {
         return getLocaleTextProvider().getText(locale, localeKey);
     }
 
     @Nullable
-    public static LocaleText getText(@NotNull String locale, @NotNull String localeKey) {
-        return getText(Locale.forLanguageTag(locale.replace('_', '-')), localeKey);
+    public static LocaleText get(@NotNull String locale, @NotNull String localeKey) {
+        return get(Locale.forLanguageTag(locale.replace('_', '-')), localeKey);
     }
 
     @Nullable
-    public static LocaleText getText(@NotNull org.spongepowered.api.entity.living.player.Player player, @NotNull String localeKey) {
-        return getText(player.getLocale(), localeKey);
+    public static LocaleText get(@NotNull org.spongepowered.api.entity.living.player.Player player, @NotNull String localeKey) {
+        return get(player.getLocale(), localeKey);
     }
 
     @Nullable
-    public static LocaleText getText(@NotNull org.bukkit.entity.Player player, @NotNull String localeKey) {
-        return getText(player.getLocale(), localeKey);
+    public static LocaleText get(@NotNull org.bukkit.entity.Player player, @NotNull String localeKey) {
+        return get(player.getLocale(), localeKey);
     }
 
     @Nullable
-    public static LocaleText getText(@NotNull net.md_5.bungee.api.connection.ProxiedPlayer player, @NotNull String localeKey) {
-        return getText(player.getLocale(), localeKey);
+    public static LocaleText get(@NotNull net.md_5.bungee.api.connection.ProxiedPlayer player, @NotNull String localeKey) {
+        return get(player.getLocale(), localeKey);
     }
 }
